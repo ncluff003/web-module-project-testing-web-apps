3772ca3f7d6be1d45f37fd879165b872fc312b51
ncluff003 <ncluff003@zohomail.com>
2023-03-11 13:42:32 -0700

    Updated code for MVP.

--- frontend/components/ContactForm.test.js
+++ frontend/components/ContactForm.test.js
@@ -3,7 +3,6 @@ import { render, screen, waitFor } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import userEvent from "@testing-library/user-event";
 import ContactForm from "./ContactForm";
-import DisplayComponent from "./DisplayComponent";
 
 test("renders without errors", () => {
   // errors have an id of error.
@@ -22,9 +21,11 @@ test("renders the contact form header", () => {
   // Arrange
   render(<ContactForm />);
   // Act
-  const header = screen.getByText("Contact Form");
+  const header = screen.queryByText(/Contact Form/i);
   // Assert
-  expect(header).toBeInTheDocument;
+  expect(header).toBeInTheDocument();
+  expect(header).toBeTruthy();
+  expect(header).toHaveTextContent(/contact form/i);
 });
 
 test("renders ONE error message if user enters less then 5 characters into firstname.", async () => {
@@ -32,26 +33,12 @@ test("renders ONE error message if user enters less then 5 characters into first
   render(<ContactForm />);
 
   // Act
-  const firstnameLabel = screen.getByText("First Name*");
-  const firstname = firstnameLabel.nextSibling;
-  await userEvent.type(firstname, "Edd");
+  // const firstnameLabel = screen.getByText("First Name*");
+  const firstname = screen.getByLabelText(/First Name*/i);
+  userEvent.type(firstname, "Edd");
 
-  const lastnameLabel = screen.getByText("Last Name*");
-  const lastname = lastnameLabel.nextSibling;
-  await userEvent.type(lastname, "Burke");
-
-  const emailLabel = screen.getByText("Email*");
-  const email = emailLabel.nextSibling;
-  await userEvent.type(email, "example@email.com");
-
-  const submitButton = screen.getByText("Submit");
-
-  await userEvent.click(submitButton);
-
-  const error = screen.getByTestId("error");
-
-  // Assert
-  await expect(error).toBeInTheDocument;
+  const errorMessages = await screen.findAllByTestId("error");
+  expect(errorMessages).toHaveLength(1);
 });
 
 test("renders THREE error messages if user enters no values into any fields.", async () => {
@@ -59,26 +46,28 @@ test("renders THREE error messages if user enters no values into any fields.", a
   render(<ContactForm />);
   // Act
   const submitButton = screen.getByText("Submit");
-  await userEvent.click(submitButton);
-  const errors = screen.queryAllByTestId("error");
-  // Assert
-  expect(errors).toHaveLength(3);
+  userEvent.click(submitButton);
+
+  await waitFor(() => {
+    const errors = screen.queryAllByTestId("error");
+    // Assert
+    expect(errors).toHaveLength(3);
+  });
 });
 
 test("renders ONE error message if user enters a valid first name and last name but no email.", async () => {
   // Arrange
   render(<ContactForm />);
   // Act
-  const firstnameLabel = screen.getByText("First Name*");
-  const firstname = firstnameLabel.nextSibling;
-  await userEvent.type(firstname, "Anthony");
-
-  const lastnameLabel = screen.getByText("Last Name*");
-  const lastname = lastnameLabel.nextSibling;
-  await userEvent.type(lastname, "Burke");
+  const firstname = screen.getByLabelText(/first name*/i);
+  const lastname = screen.getByLabelText(/last name*/i);
+  userEvent.type(firstname, "Anthony");
+  userEvent.type(lastname, "Burke");
 
   const submitButton = screen.getByText("Submit");
-  await userEvent.click(submitButton);
+  userEvent.click(submitButton);
+
+  await waitFor(() => {});
   const errors = screen.queryAllByTestId("error");
   // Assert
   expect(errors).toHaveLength(1);
@@ -88,62 +77,59 @@ test('renders "email must be a valid email address" if an invalid email is enter
   // Arrange
   render(<ContactForm />);
   // Act
-  const firstnameLabel = screen.getByText("First Name*");
-  const firstname = firstnameLabel.nextSibling;
-  await userEvent.type(firstname, "Anthony");
+  const firstname = screen.getByLabelText(/first name*/i);
+  userEvent.type(firstname, "Anthony");
 
-  const lastnameLabel = screen.getByText("Last Name*");
-  const lastname = lastnameLabel.nextSibling;
-  await userEvent.type(lastname, "Burke");
+  const lastname = screen.getByLabelText(/last name*/i);
+  userEvent.type(lastname, "Burke");
 
-  const emailLabel = screen.getByText("Email*");
-  const email = emailLabel.nextSibling;
-  await userEvent.type(email, "example");
+  const email = screen.getByLabelText(/Email*/);
+  userEvent.type(email, "example");
 
   const submitButton = screen.getByText("Submit");
-  await userEvent.click(submitButton);
-  const errors = screen.getByTestId("error");
-  // Assert
-  expect(errors).toHaveTextContent("email must be a valid email address");
+  userEvent.click(submitButton);
+
+  await waitFor(() => {
+    const errors = screen.getByTestId("error");
+    // Assert
+    expect(errors).toHaveTextContent("email must be a valid email address");
+  });
 });
 
 test('renders "lastName is a required field" if an last name is not entered and the submit button is clicked', async () => {
   // Arrange
   render(<ContactForm />);
   // Act
-  const firstnameLabel = screen.getByText("First Name*");
-  const firstname = firstnameLabel.nextSibling;
-  await userEvent.type(firstname, "Anthony");
+  const firstname = screen.getByLabelText(/first name*/i);
+  userEvent.type(firstname, "Anthony");
 
-  const lastnameLabel = screen.getByText("Last Name*");
-  const lastname = lastnameLabel.nextSibling;
+  const lastname = screen.getByLabelText(/last name*/i);
 
-  const emailLabel = screen.getByText("Email*");
-  const email = emailLabel.nextSibling;
-  await userEvent.type(email, "example@email.com");
+  const email = screen.getByLabelText(/Email*/);
+  userEvent.type(email, "example@email.com");
 
   const submitButton = screen.getByText("Submit");
-  await userEvent.click(submitButton);
-  const errors = screen.getByTestId("error");
-  // Assert
-  expect(errors).toHaveTextContent("lastName is a required field");
+  userEvent.click(submitButton);
+
+  await waitFor(() => {
+    const errors = screen.getByTestId("error");
+    // Assert
+    expect(errors).toHaveTextContent("lastName is a required field");
+  });
 });
 
 test("renders all firstName, lastName and email text when submitted. Does NOT render message if message is not submitted.", async () => {
   // Arrange
   render(<ContactForm />);
   // Act
-  const firstnameLabel = screen.getByText("First Name*");
-  const firstname = firstnameLabel.nextSibling;
-  await userEvent.type(firstname, "Anthony");
+  const firstname = screen.getByLabelText(/first name*/i);
+  userEvent.type(firstname, "Anthony");
 
-  const lastnameLabel = screen.getByText("Last Name*");
-  const lastname = lastnameLabel.nextSibling;
-  await userEvent.type(lastname, "Burke");
+  const lastname = screen.getByLabelText(/last name*/i);
+  userEvent.type(lastname, "Burke");
 
-  const emailLabel = screen.getByText("Email*");
-  const email = emailLabel.nextSibling;
-  await userEvent.type(email, "example@email.com");
+  const email = screen.getByLabelText(/Email*/);
+  userEvent.type(email, "example@email.com");
 
   const submitButton = screen.getByText("Submit");
   await userEvent.click(submitButton);
@@ -164,21 +150,17 @@ test("renders all fields text when all fields are submitted.", async () => {
   // Arrange
   render(<ContactForm />);
   // Act
-  const firstnameLabel = screen.getByText("First Name*");
-  const firstname = firstnameLabel.nextSibling;
-  await userEvent.type(firstname, "Anthony");
+  const firstname = screen.getByLabelText(/first name*/i);
+  userEvent.type(firstname, "Anthony");
 
-  const lastnameLabel = screen.getByText("Last Name*");
-  const lastname = lastnameLabel.nextSibling;
-  await userEvent.type(lastname, "Burke");
+  const lastname = screen.getByLabelText(/last name*/i);
+  userEvent.type(lastname, "Burke");
 
-  const emailLabel = screen.getByText("Email*");
-  const email = emailLabel.nextSibling;
-  await userEvent.type(email, "example@email.com");
+  const email = screen.getByLabelText(/Email*/);
+  userEvent.type(email, "example@email.com");
 
-  const messageLabel = screen.getByText("Message");
-  const message = messageLabel.nextSibling;
-  await userEvent.type(message, "Hello world!");
+  const message = screen.getByLabelText(/Message/i);
+  userEvent.type(message, "Hello world!");
 
   const submitButton = screen.getByText("Submit");
   await userEvent.click(submitButton);
